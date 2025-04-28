1fb9259263327f091a9d620cb4fe17e1db0e8986
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-04-28 13:19:10 +0500

    add dead zone

---
+++ Assets/Main/Scripts/Core/DeadZone.cs
@@ -0,0 +1,20 @@
+﻿using TestGame.Core.Player.Markers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TestGame.Core
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(BoxCollider))]
+    public class DeadZone : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out PlayerMarker _))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
